31134bf6d9f24fd671e87790a87d0342ce844fda
ANIL Charif Zoher <anilcharif06@gmail.com>
2022-11-15 11:50:43 +0100

    Create header.inc.php

---
+++ Pages/header.inc.php
@@ -0,0 +1,9 @@
+
+<header>
+        <h1>
+           
+                <img src="./favicon/favicon-32x32.png" alt="logo" loading="lazy" aria-hidden="true">
+          
+            Votre site de vente  <strong> PC Gamer </strong>
+        </h1>
+    </header>
